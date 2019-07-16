db4e143657458ac63d7a2fb1ac29c635d82d153a
Andrew <dev.andrewhan@gmail.com>
2019-07-15 23:38:00 -0400

    Updates

---
+++ precache-manifest.c947aadd1f9c42b802ee75925428d7af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd963cb74f769c5e439c543c2c0f471",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8f2668f00ea2ec6bc18e",
+    "url": "/monsters-rolodex/static/css/main.630513f3.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/monsters-rolodex/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "8f2668f00ea2ec6bc18e",
+    "url": "/monsters-rolodex/static/js/main.387360e8.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
